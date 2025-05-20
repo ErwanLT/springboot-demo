f047b95855ac314a925b925364797931c560a01d
LE TUTOUR Erwan <le.tutour.erwan@groupagrica.com>
2025-05-20 09:02:45 +0200

    init ldap

---
+++ security-tutorial/ldap-tutorial/src/main/java/fr/eletutour/ldaptutorial/LdapTutorialApplication.java
@@ -0,0 +1,13 @@
+package fr.eletutour.ldaptutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdapTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LdapTutorialApplication.class, args);
+    }
+
+}
---
+++ security-tutorial/ldap-tutorial/src/test/java/fr/eletutour/ldaptutorial/LdapTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eletutour.ldaptutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LdapTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
