ee98afdad556dab26578b5438eedbdcb21b510c3
Erwan <erwanletutour.elt@gmail.com>
2025-06-05 15:05:13 +0200

    ldap tutorial

---
+++ security-tutorial/ldap-tutorial/src/main/java/fr/eletutour/ldap/LdapTutorialApplication.java
@@ -0,0 +1,11 @@
+package fr.eletutour.ldap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdapTutorialApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LdapTutorialApplication.class);
+    }
+}
