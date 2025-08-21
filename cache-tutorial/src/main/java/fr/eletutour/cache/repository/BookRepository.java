488e97707400f13e2908c539c7173ff94d257839
Erwan LE TUTOUR <erwanletutour.elt@gmail.com>
2025-08-21 13:30:39 +0200

    cache tutorial

---
+++ cache-tutorial/src/main/java/fr/eletutour/cache/repository/BookRepository.java
@@ -0,0 +1,11 @@
+package fr.eletutour.cache.repository;
+
+import fr.eletutour.cache.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+    Book findByIsbn(String isbn);
+}
