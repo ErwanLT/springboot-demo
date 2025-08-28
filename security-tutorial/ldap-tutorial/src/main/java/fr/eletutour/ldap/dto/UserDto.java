cf7cdafb6bf125ed01dc735b677e3d88f4f28291
Erwan LE TUTOUR <erwanletutour.elt@gmail.com>
2025-08-28 10:44:01 +0200

    LDAP tutorial
    gestion de groupe

--- security-tutorial/ldap-tutorial/src/main/java/fr/eletutour/ldap/dto/UserDto.java
+++ security-tutorial/ldap-tutorial/src/main/java/fr/eletutour/ldap/dto/UserDto.java
@@ -1,4 +1,27 @@
 package fr.eletutour.ldap.dto;
 
-public record UserDto(String fullName, String uid, String email) {
+import java.util.List;
+
+public class UserDto {
+    private final String fullName;
+    private final String uid;
+    private final String email;
+
+    public UserDto(String fullName, String uid, String email) {
+        this.fullName = fullName;
+        this.uid = uid;
+        this.email = email;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
--- security-tutorial/ldap-tutorial/src/main/java/fr/eletutour/ldap/service/LdapService.java
+++ security-tutorial/ldap-tutorial/src/main/java/fr/eletutour/ldap/service/LdapService.java
@@ -1,11 +1,19 @@
 package fr.eletutour.ldap.service;
 
+import fr.eletutour.ldap.dto.NewUserDto;
 import fr.eletutour.ldap.dto.UserDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ldap.core.AttributesMapper;
+import org.springframework.ldap.core.ContextMapper;
+import org.springframework.ldap.core.DirContextAdapter;
+import org.springframework.ldap.core.DirContextOperations;
 import org.springframework.ldap.core.LdapTemplate;
+import org.springframework.ldap.support.LdapNameBuilder;
 import org.springframework.stereotype.Service;
 
+import javax.naming.Name;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -28,4 +36,40 @@ public class LdapService {
                 )
         );
     }
+
+    public void createUser(NewUserDto userDto) {
+        Name dn = LdapNameBuilder.newInstance()
+                .add("ou", "users")
+                .add("cn", userDto.getFullName())
+                .build();
+
+        DirContextAdapter context = new DirContextAdapter(dn);
+        context.setAttributeValues("objectclass", new String[]{"top", "inetOrgPerson"});
+        context.setAttributeValue("cn", userDto.getFullName());
+        String[] names = userDto.getFullName().split(" ");
+        context.setAttributeValue("sn", names.length > 1 ? names[names.length - 1] : userDto.getFullName());
+        context.setAttributeValue("uid", userDto.getUid());
+        context.setAttributeValue("mail", userDto.getEmail());
+        context.setAttributeValue("userPassword", userDto.getPassword());
+
+        ldapTemplate.bind(context);
+    }
+
+    public void addUserToGroup(String userFullName, String groupName) {
+        Name userDn = LdapNameBuilder.newInstance("dc=example,dc=com")
+                .add("ou", "users")
+                .add("cn", userFullName)
+                .build();
+
+        Name groupDn = LdapNameBuilder.newInstance()
+                .add("ou", "groups")
+                .add("cn", groupName)
+                .build();
+
+        DirContextOperations group = ldapTemplate.lookupContext(groupDn);
+        group.addAttributeValue("uniqueMember", userDn.toString());
+        ldapTemplate.modifyAttributes(group);
+    }
+
+    
 }
