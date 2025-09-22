01bc6421dd5a69dcb466ddacdda6a5dd3c4fcabd
Erwan LE TUTOUR <erwanletutour.elt@gmail.com>
2025-09-22 18:20:41 +0200

    file upload tutorial

---
+++ web-layer/fileUploadDownload-tutorial/src/main/java/fr/eletutour/FileUploadDownloadApplication.java
@@ -0,0 +1,11 @@
+package fr.eletutour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileUploadDownloadApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FileUploadDownloadApplication.class);
+    }
+}
---
+++ web-layer/fileUploadDownload-tutorial/src/main/java/fr/eletutour/controller/FileStorageController.java
@@ -0,0 +1,55 @@
+package fr.eletutour.controller;
+
+import fr.eletutour.service.FileStorageService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+@RestController
+public class FileStorageController {
+
+    private final FileStorageService fileStorageService;
+
+    public FileStorageController(FileStorageService fileStorageService) {
+        this.fileStorageService = fileStorageService;
+    }
+
+    @PostMapping("/uploadFile")
+    public ResponseEntity<Void> uploadFile(@RequestParam("file") MultipartFile file) {
+        fileStorageService.storeFile(file);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/uploadMultipleFiles")
+    public void uploadMultipleFiles(@RequestParam("files") MultipartFile[] files) {
+        Arrays.stream(files).forEach(fileStorageService::storeFile);
+    }
+
+    @GetMapping("/downloadFile/{fileName:.+}")
+    public ResponseEntity<Resource> downloadFile(@PathVariable String fileName, HttpServletRequest request) {
+        Resource resource = fileStorageService.loadFileAsResource(fileName);
+
+        String contentType = null;
+        try {
+            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+        } catch (IOException ex) {
+            //
+        }
+
+        if (contentType == null) {
+            contentType = "application/octet-stream";
+        }
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
+    }
+}
