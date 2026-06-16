1abd6e191deb6b6a8c340665d79eca12d9ec7af2
Erwan LE TUTOUR <erwanletutour.elt@gmail.com>
2026-06-16 14:55:19 +0200

    add test by properties tutorial

---
+++ operations/testing-tutorial/properties-test-tutorial/src/main/java/fr/eletutour/properties/testing/PropertieTestingTutorialApplication.java
@@ -0,0 +1,11 @@
+package fr.eletutour.properties.testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertieTestingTutorialApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PropertieTestingTutorialApplication.class, args);
+    }
+}
